f39f61028de74806aa4b1dc3f4a2a2d8be49808c
alessio-civitella <alessio.civitella19@gmail.com>
2024-12-03 15:18:15 +0100

    Feat: copia codice vecchio

---
+++ data.js
@@ -0,0 +1,35 @@
+const posts = [
+    {
+      titolo: "L'arte del coding",
+      contenuto: "Scopri come migliorare le tue competenze di programmazione con consigli e best practice.",
+      immagine: "/ciambellone.jpeg",
+      tags: ["coding", "programmazione", "sviluppo web"]
+    },
+    {
+      titolo: "Viaggio tra le stelle",
+      contenuto: "Un'esplorazione delle più affascinanti scoperte astronomiche recenti.",
+      immagine: "/cracker_barbabietola.jpeg",
+      tags: ["astronomia", "scienza", "universo"]
+    },
+    {
+      titolo: "Cucina gourmet a casa tua",
+      contenuto: "Ricette semplici e veloci per portare la cucina stellata nella tua cucina.",
+      immagine: "/pane_fritto_dolce.jpeg",
+      tags: ["cucina", "gourmet", "ricette"]
+    },
+    {
+      titolo: "Fitness: allenarsi a casa",
+      contenuto: "Ecco una guida su come mantenerti in forma senza uscire di casa.",
+      immagine: "/pasta_barbabietola.jpeg",
+      tags: ["fitness", "salute", "benessere"]
+    },
+    {
+      titolo: "I migliori libri del 2024",
+      contenuto: "Una selezione dei libri imperdibili di quest'anno, da leggere assolutamente.",
+      immagine: "/torta_paesana.jpeg",
+      tags: ["libri", "lettura", "2024"]
+    }
+  ];
+  
+
+  module.exports = posts;
